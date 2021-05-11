fc938ecb20c86ccafe356a772bf0e7fee9ab82ef
Hui <34175635+mindtheworld@users.noreply.github.com>
2021-05-11 14:00:15 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Test Me!</h1>
+      <h2>Try out new things!</h2>
+    </div>
+  );
+};
+
+export default App;
